58b9d1177f5cb1575a627f452f22df2fcae8d376
basit <basitmohammed362@gmail.com>
2024-07-06 14:34:45 +0000

    hello

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -28,6 +28,47 @@ export const createProductCtrl = expressAsyncHandler(async (req, res) => {
 
 // a controller to fetch all products that exist
 export const allProductCtrl = expressAsyncHandler(async (req, res) => {
-  const products = await Product.find();
-  return res.status(200).json(products);
-});
+  let productQuery = Product.find();
+
+  if (req.query.name) {
+    productQuery = productQuery.find({
+      name: {
+        $regex: req.query.name,
+        $options: 'i'
+      }
+    });
+  }
+
+  if(req.query.brand){
+    productQuery = productQuery.find({
+      brand: {
+        $regex: req.query.brand,
+        $options: 'i'
+      }
+    });
+  }
+
+  if(req.query.size){
+    productQuery = productQuery.find(
+      {sizes: req.query.size}
+    )};
+
+    if(req.query.category){
+      productQuery = productQuery.find({
+        category: { $regex: req.query.category, $options: 'i' }
+      });
+    }
+    if(req.query.color){
+      productQuery = productQuery.find({
+        colors: { $regex: req.query.color, $options: 'i' }
+      });
+    }
+    
+  const products = await productQuery;
+
+  return res.status(200).json({
+      msg: "success",
+      products
+    });
+  });
+
